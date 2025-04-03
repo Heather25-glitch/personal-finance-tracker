410391828786e3843a1eb5488f5bfb94b755dc41
Heather Selim <heatherselim25@gmail.com>
2025-04-04 01:39:00 +0200

    fixed the urls error

--- finances/urls.py
+++ finances/urls.py
@@ -1,13 +1,15 @@
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import IncomeViewSet, ExpenseViewSet, SavingsGoalViewSet, TotalIncomeView
 
+# Registering the ViewSets with the router
 router = DefaultRouter()
-router.register(r'income', IncomeViewSet)
-router.register(r'expense', ExpenseViewSet)
-router.register(r'savings_goal', SavingsGoalViewSet)
+router.register(r'income', IncomeViewSet, basename='income')
+router.register(r'expense', ExpenseViewSet, basename='expense')
+router.register(r'savings_goal', SavingsGoalViewSet, basename='savings_goal')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),  
     path('total-income/', TotalIncomeView.as_view(), name='total-income'),
 ]
